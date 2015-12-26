1cc95caa4051ffa5ca154b946e8cae04fdea19e3
Gustavo Falco <comfortablynumb84@gmail.com>
2015-12-26 18:21:08 -0300

    Added basic files.

---
+++ src/KernelInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the kernel package.
+ *
+ * (c) Gustavo Falco <comfortablynumb84@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace IronEdge\Component\Kernel;
+
+
+interface KernelInterface
+{
+
+}
