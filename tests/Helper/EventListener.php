72a779b0e4e3616ddda657ea2114ca1f73217605
Gustavo Falco <comfortablynumb84@gmail.com>
2015-12-28 22:30:46 -0300

    Added missing files.

---
+++ tests/Helper/EventListener.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the kernel package.
+ *
+ * (c) Gustavo Falco <comfortablynumb84@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace IronEdge\Component\Kernel\Test\Helper;
+
+
+/*
+ * @author Gustavo Falco <comfortablynumb84@gmail.com>
+ */
+use IronEdge\Component\Config\ConfigInterface;
+use IronEdge\Component\Kernel\Config\ProcessorInterface;
+use IronEdge\Component\Kernel\Kernel;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class EventListener
+{
+    public static $called = false;
+
+    public function handle()
+    {
+        self::$called = true;
+    }
+}
